2b15bf07fded70edb3bd6cc7cad0cb8b0b222a55
Erik Lee <eriklee113@gmail.com>
2023-12-14 23:36:32 +0000

    day12: part2
    sadly had to basically rewrite everything as I thought I might. at least
    it seems a bit quicker as DP than brute force.

--- src/day12.cpp
+++ src/day12.cpp
@@ -2,27 +2,17 @@
 
 #include <algorithm>
 #include <tuple>
+#include <deque>
 #include <fmt/format.h>
-#include <fmt/color.h>
 #include <numeric>
 #include <vector>
 #include <string_view>
-#include <set>
+#include <map>
 
 enum class condition {
   operational, // .
   damaged,     // #
   unknown,     // ?
-};
-
-struct rle_elem {
-  condition value;
-  int length;
-};
-
-struct rle_line {
-  std::vector<rle_elem> line;
-  std::vector<int> damaged_runs;
 };
 
 condition parseCond(char c) {
@@ -34,166 +24,100 @@ condition parseCond(char c) {
   __builtin_unreachable();
 }
 
-rle_line parseLine(std::string_view line) {
-  rle_line result;
+struct raw_line {
+  std::deque<condition> row;
+  std::deque<int> damaged_runs;
+  bool operator==(const raw_line&) const = default;
+  auto operator<=>(const raw_line&) const = default;
+};
 
-  char last = line.front();
-  int run_length = 0;
+raw_line parseRawLine(std::string_view line) {
+  raw_line res;
+
   while (line.front() != ' ') {
     auto curr = line.front();
-    if (curr == last) {
-      run_length++;
-    } else {
-      result.line.emplace_back(rle_elem{.value = parseCond(last), .length = run_length});
-      last = curr;
-      run_length = 1;
-    }
+    res.row.push_back(parseCond(curr));
     line.remove_prefix(1);
   }
-  result.line.emplace_back(rle_elem{.value = parseCond(last), .length = run_length});
-  
 
   utils::eatChar(' ', line);
   while (!line.empty()) {
-    result.damaged_runs.push_back(utils::parseInt(line));
+    res.damaged_runs.push_back(utils::parseInt(line));
     utils::eatLiteral(",", line);
   }
 
-  return result;
-}
-
-bool validLine(const rle_line& l) {
-  auto l_it = l.line.begin();
-  auto d_it = l.damaged_runs.begin();
-  while (l_it != l.line.end() && d_it != l.damaged_runs.end()) {
-    if (l_it->value == condition::damaged) {
-      if (l_it->length != *d_it) return false;
-      ++d_it; // it was fine, just increment everything
-    }
-    ++l_it;
-  }
-  while (l_it != l.line.end()) {
-    auto cond = (l_it++)->value;
-
-    if (cond == condition::damaged) return false;
-  }
-  return (l_it == l.line.end() && d_it == l.damaged_runs.end());
-}
-
-void canonicalizeLine(std::vector<rle_elem>& l) {
-  auto l_it = l.begin();
-  while (l_it != l.end()) {
-    if (auto next_it = l_it + 1; next_it != l.end()) {
-      if (l_it->value == next_it->value) {
-        next_it->length += l_it->length;
-        l_it = l.erase(l_it);
-      } else {
-        ++l_it;
-      }
-    } else {
-      ++l_it;
-    }
-  }
-}
-
-
-std::vector<rle_line> chooseUnknown(const rle_line& inp) {
-  // if we have an operational run we can just drop it (return only one element)
-  // if we have a damaged run we can see if it matches the next damaged value
-  //  - if so, remove it, and ensure the next value (if it exists) is operational, either already
-  //    or by changing one element of the next run of unknown results (return only one element)
-  //  - if not, and we can't extend return nothing
-  // if we have an unknown run there are 2 cases for what might follow, operational or damaged
-  //   if the follower is operational we can either:
-  //      fill in the next damaged value (if possible)
-  //        potentially followed by a 1-run of operational if there's space * return value.first
-  //      mark the cell as operational (either with a new run or extending the past) and iterate once * return value.second
-  //   if the follower is damaged and we need the values to get to the next damaged value, we can return a 
-  //   list of lines with 0-length operational values
-  //   if the follower is damaged but we have enough we can return a list of valid damaged values that fit in with an operational follower
-  return {};
-}
-
-std::pair<int, int> sumElems(const std::vector<rle_elem>& inp) {
-  int usum = 0;
-  int dsum = 0;
-  for (const auto& elem : inp) {
-    if (elem.value == condition::unknown) usum += elem.length;
-    if (elem.value == condition::damaged) dsum += elem.length;
-  }
-  return {usum, dsum};
-}
-
-int sumDamaged(const std::vector<int>& inp) {
-  return std::accumulate(inp.begin(), inp.end(), 0);
-}
-
-std::vector<uint64_t> filterUnknownsWithDamages(const int damageCount, const int unknownCount) {
-  std::vector<uint64_t> result;
-  for (uint x = 0; x < (1 << unknownCount); x++) {
-    if (std::popcount(x) == damageCount) result.push_back(x);
-  }
-  return result;
-}
-
-std::vector<rle_elem> fillLine(const std::vector<rle_elem>& inp, int potentialFillings) {
-  std::vector<rle_elem> res;
-  for (auto e : inp) {
-    if (e.value == condition::unknown) {
-      while (e.length-- > 0) {
-        if (potentialFillings & 1) {
-          res.push_back(rle_elem{condition::damaged, 1});
-        } else {
-          res.push_back(rle_elem{condition::operational, 1});
-        }
-        potentialFillings >>= 1;
-      }
-    } else {
-      res.push_back(e);
-    }
-  }
-  canonicalizeLine(res);
   return res;
 }
 
-int bruteForce(rle_line inp) {
-  auto [unknownCount, damagedCount] = sumElems(inp.line);
-  auto requiredDamages = sumDamaged(inp.damaged_runs);
-  auto potentialFillings = filterUnknownsWithDamages(requiredDamages - damagedCount, unknownCount);
-
-  int result = 0;
-  const auto originalLine = inp.line;
-  for (auto pf : potentialFillings) {
-    inp.line = fillLine(originalLine, pf);
-    if (validLine(inp)) result++;
+std::string fmt_row(const std::deque<condition>& row) {
+  std::string result;
+  for (const auto c : row) {
+    if (c == condition::damaged) result.push_back('#');
+    else if (c == condition::operational) result.push_back('.');
+    else if (c == condition::unknown) result.push_back('?');
   }
 
   return result;
 }
 
+template<bool Debug = false>
+uint64_t calculatePossibilities(std::map<raw_line, uint64_t>& cache, raw_line inp) {
+  auto [cache_it, unseen]  = cache.emplace(inp, 0);
+  auto& result = cache_it->second;
+  if constexpr (Debug) fmt::println("calculatePossibilities: {} - {} = {}", fmt_row(inp.row), fmt::join(inp.damaged_runs, ","), result);
+  if (!unseen) return cache_it->second;
+
+  if (inp.row.empty() && !inp.damaged_runs.empty()) { result = 0; return result; }
+  if (inp.damaged_runs.empty()) {
+    if (std::any_of(inp.row.begin(), inp.row.end(), [](const condition c) { return c == condition::damaged; }))
+        result = 0;
+    else result = 1;
+    return result;
+  }
+
+  if (inp.row.front() == condition::operational) {
+    while(!inp.row.empty() && inp.row.front() == condition::operational) { inp.row.pop_front(); }
+    result = calculatePossibilities<Debug>(cache, inp);
+  } else if (inp.row.front() == condition::damaged) {
+    auto damageCount = inp.damaged_runs.front();
+    inp.damaged_runs.pop_front();
+    result = 1;
+    while (damageCount-- > 0) {
+      if (inp.row.empty()) { result = 0; break; }
+      if (inp.row.front() == condition::operational) { result = 0; break; }
+      inp.row.pop_front();
+    }
+    if (!inp.row.empty() && inp.row.front() == condition::damaged) { result = 0; } // next is damaged - wrong
+    if (!inp.row.empty() && inp.row.front() == condition::unknown) { inp.row.front() = condition::operational; } // force next choice to be operational
+    if (result) {
+      result = calculatePossibilities<Debug>(cache, inp);
+    }
+  } else { // unknown!
+    auto op_inp = inp;
+    op_inp.row.pop_front();
+    result = calculatePossibilities<Debug>(cache, op_inp); // if we say it's operational
+    // otherwise it's damaged
+    inp.row.front() = condition::damaged;
+    result += calculatePossibilities<Debug>(cache, inp);
+  }
+
+  return result;
+}
+
+raw_line part2ize(raw_line& inp) {
+  raw_line res = inp;
+
+  for (int i = 0; i < 4; ++i) {
+    res.row.push_back(condition::unknown);
+    res.row.insert(res.row.end(), inp.row.begin(), inp.row.end());
+
+    res.damaged_runs.insert(res.damaged_runs.end(), inp.damaged_runs.begin(), inp.damaged_runs.end());
+  }
+  return res;
+}
+
+
 void test() {
-  auto ex = parseLine("???.###.. 1,1,3");
-  utils::Assert(ex.damaged_runs.size() == 3);
-  utils::Assert(ex.damaged_runs[0] == 1);
-  utils::Assert(ex.damaged_runs[1] == 1);
-  utils::Assert(ex.damaged_runs[2] == 3);
-
-  utils::Assert(ex.line.size() == 4);
-  utils::Assert(ex.line[0].value == condition::unknown);
-  utils::Assert(ex.line[0].length == 3);
-  utils::Assert(ex.line[1].value == condition::operational);
-  utils::Assert(ex.line[1].length == 1);
-  utils::Assert(ex.line[2].value == condition::damaged);
-  utils::Assert(ex.line[2].length == 3);
-  utils::Assert(ex.line[3].value == condition::operational);
-  utils::Assert(ex.line[3].length == 2);
-
-  utils::Assert(validLine(parseLine("#.#.### 1,1,3")));
-  utils::Assert(validLine(parseLine(".#.....#....###. 1,1,3")));
-  utils::Assert(validLine(parseLine("#.###.#.###### 1,3,1,6")));
-  utils::Assert(validLine(parseLine("###..##.....# 3,2,1")));
-  utils::Assert(!validLine(parseLine("###..##..#..# 3,2,1")));
-  utils::Assert(!validLine(parseLine("###..##..#..# 4,2,1")));
 }
 
 int main(int argc, char **argv) {
@@ -202,12 +126,20 @@ int main(int argc, char **argv) {
   utils::LineReader lr{"inp/day12.txt"};
 
   uint64_t p1 = 0;
+  uint64_t p2 = 0;
+  std::map<raw_line, uint64_t> cache;
   while (auto line = lr.getLine()) {
-    auto rle_line = parseLine(*line);
-    auto res = bruteForce(rle_line);
+    cache.clear();
+    auto raw_line = parseRawLine(*line);
+    auto res = calculatePossibilities(cache, raw_line);
     p1 += res;
-    fmt::println("{} -> {}", *line, res);
+
+    raw_line = part2ize(raw_line);
+    res = calculatePossibilities(cache, raw_line);
+    p2 += res;
+
+    // fmt::println("{} -> {}", *line, res);
   }
   fmt::println("Day12: Part 1: {}", p1);
-  // fmt::println("Day11: Part 2: {}", p2);
+  fmt::println("Zay11: Part 2: {}", p2);
 }
